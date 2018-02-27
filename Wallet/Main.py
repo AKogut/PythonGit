a07afec8dd76eb4fccb95b18b3278a638721c865
AKogut <andruhakogut@gmail.com>
2018-02-27 22:39:10 +0200

    Wallet

--- Wallet/Main.py
+++ Wallet/Main.py
@@ -1,24 +1,42 @@
 class Wallet:
     balance = 130
+    game = 0
+    entertainment = 0
+    meal = 0
+    other = 0
+    ctg = [game, entertainment, meal, other]
 
 
 wallet = Wallet()
-print("refill")
-print("withdraw")
-print("report")
-print("balance")
-print("end")
 while True:
-    actions = input("Choose actions: ")
+    print("<----------------------->")
+    print("refill")
+    print("withdraw")
+    print("report")
+    print("balance")
+    print("end")
+    actions = input("Choose actions: \n")
     if actions == 'refill':
-        print("okey")
+        refill = float(input("Enter sum for refill: "))
+        wallet.balance += refill
     elif actions == 'withdraw':
-        print("")
+        print("Category <----------> ")
+        print("game")
+        print("entertainment")
+        print("meal")
+        print("other")
+        category = input("Choose category: ")
+        if category == 'game':
+            wallet.ctg[0] = float(input("Enter a sum for withdraw in category game: "))
+            wallet.balance -= wallet.ctg[0]
+        else:
+            print("Error")
+
     elif actions == 'report':
         print("")
     elif actions == 'balance':
         print(wallet.balance)
-    elif actions == 'end':
+    elif actions == 'exit':
         break
     else:
         print("Error choose another actions")
