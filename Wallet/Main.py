76b1a827753222a77f9f9291b42c83eb93a42518
AKogut <andruhakogut@gmail.com>
2018-02-28 21:31:49 +0200

    Wallet

--- Wallet/Main.py
+++ Wallet/Main.py
@@ -1,5 +1,6 @@
 class Wallet:
     balance = 130
+    refill = 0
     game = 0
     entertainment = 0
     meal = 0
@@ -14,11 +15,11 @@ while True:
     print("withdraw")
     print("report")
     print("balance")
-    print("end")
+    print("exit")
     actions = input("Choose actions: \n")
     if actions == 'refill':
-        refill = float(input("Enter sum for refill: "))
-        wallet.balance += refill
+        wallet.refill = float(input("Enter sum for refill: "))
+        wallet.balance += wallet.refill
     elif actions == 'withdraw':
         print("Category <----------> ")
         print("game")
@@ -29,11 +30,25 @@ while True:
         if category == 'game':
             wallet.ctg[0] = float(input("Enter a sum for withdraw in category game: "))
             wallet.balance -= wallet.ctg[0]
+        elif category == 'entertainment':
+            wallet.ctg[1] = float(input("Enter a sum for withdraw in category entertainment: "))
+            wallet.balance -= wallet.ctg[1]
+        elif category == 'meal':
+            wallet.ctg[2] = float(input("Enter a sum for withdraw in category meal: "))
+            wallet.balance -= wallet.ctg[2]
+        elif category == 'other':
+            wallet.ctg[3] = float(input("Enter a sum for withdraw in category other: "))
+            wallet.balance -= wallet.ctg[3]
         else:
             print("Error")
-
     elif actions == 'report':
-        print("")
+        print("Your balance %.2f $" % wallet.balance)
+        print("You refill for: %.2f $" % wallet.refill)
+        print("You withdraw for a category: ")
+        print("     For game: %.2f $" % wallet.ctg[0])
+        print("     For entertainment: %.2f $" % wallet.ctg[1])
+        print("     For meal: %.2f  $" % wallet.ctg[2])
+        print("     For other: %.2f  $" % wallet.ctg[3])
     elif actions == 'balance':
         print(wallet.balance)
     elif actions == 'exit':
